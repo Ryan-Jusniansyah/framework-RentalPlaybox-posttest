c1741696408ce8ae88c4145fb705f0ca80a09792
J_Ryan <114414641+Ryan-Jusniansyah@users.noreply.github.com>
2024-09-30 00:06:39 +0800

    Posttest 3

--- project_playbox/playbox/playbox2/admin.py
+++ project_playbox/playbox/playbox2/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 
 # Register your models here.
+
+from .models.admins import Admins
+from .models.customers import Customers
+from .models.devices import Devices
+from .models.users import Users
+
+class adminAdmins(admin.ModelAdmin):
+    list_display = ('noAdmin', 'namaAdmin', 'emailAdmin', 'nomorHpAdmin', 'alamatAdmin')
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(self, obj, form, change)
+
+        user, created = Users.objects.get_or_create(username=obj.noAdmin, defaults={
+            'password' : make_password('default_password'),
+            'role': Users.ADMIN
+        })
+
+        if not created:
+            user.role = Users.ADMIN
+            user.save()
+
+admin.site.register(Admins, adminAdmins)
+
+admin.site.register(Customers)
+admin.site.register(Devices)
+admin.site.register(Users)

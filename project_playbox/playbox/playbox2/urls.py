c835fb19d5cb0e8392ff3d241ed09e27d90577ac
J_Ryan <114414641+Ryan-Jusniansyah@users.noreply.github.com>
2024-10-16 19:01:25 +0800

    Posttest 4

--- project_playbox/playbox/playbox2/urls.py
+++ project_playbox/playbox/playbox2/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("about/", views.about),
     path("", views.homepage),
     path("rent/", views.rent),
+    path("device/", views.device_index, name='device_index'),
+    path('device/create/', views.device_create, name='device_create'),
+    path("device/update/<int:device_id>", views.device_update, name="device_update"),
+    path("device/delete/<int:device_id>", views.device_delete, name="device_delete")
 ]
--- project_playbox/playbox/playbox2/views.py
+++ project_playbox/playbox/playbox2/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Devices
+from django.contrib import messages
+from .forms import DevicesForm
+from django.http import JsonResponse
+from django.db.models import Q
 
 # Views homepage.
 def homepage(request):
@@ -11,3 +16,55 @@ def about(request):
 # Views rent
 def rent(request):
     return render(request, "homepage/rent.html")
+
+# READ Device
+def device_index(request):
+    query = request.GET.get('q')
+    devices = Devices.objects.all()
+    if query:
+        devices = Devices.objects.filter(
+            Q(noDevice__icontains=query) |
+            Q(namaDevice__icontains=query) |
+            Q(fasilitas__icontains=query) |
+            Q(bonus__icontains=query) |
+            Q(tanggalMasuk__icontains=query)
+        )
+    else:
+        devices = Devices.objects.all()
+    return render(request, 'device/index.html', {'devices': devices, 'query':query})
+
+# Create Device
+def device_create(request):
+    if request.method == 'POST':
+        form = DevicesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Devices berhasil ditambahkan!')
+            return redirect('device_index') 
+    else:
+        form = DevicesForm()
+
+    return render(request, 'device/create.html', {'form': form})
+
+# UPDATE Device
+def device_update(request, device_id):
+    device = get_object_or_404(Devices, id=device_id)
+    if request.method == 'POST':
+        form = DevicesForm(request.POST, instance=device)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Data Device Berhasil Diubah!')
+            return redirect('device_index')
+    else:
+        form = DevicesForm(instance=device)
+    return render(request, 'device/update.html', {'form': form, 'device': device})
+
+# DELETE Device
+def device_delete(request, device_id):
+    device = get_object_or_404(Devices, id=device_id)
+    device.delete()
+    messages.success(request, 'Data Device berhasil dihapus')
+    return JsonResponse({'success': True})
+
+
+
